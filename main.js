ffbc457e144b31819894fe56ee74ac3be604a6da
rahmona-henry <someone@devacademy.co.nz>
2016-02-04 21:13:59 +1300

    Add twitter widget

--- main.js
+++ main.js
@@ -14,16 +14,9 @@ $(document).ready(function() {
 
 twttr.widgets.createShareButton(
   "https:\/\/dev.twitter.com\/web\/tweet-button",
-  document.getElementById("tweet-container"),
-  {
-    size: "large",
-    via: "twitterdev",
-    related: "twitterapi,twitter",
-    text: "custom share text",
-    hashtags: "example,demo"
-  }
+  document.getElementById("section")
+ 
 );
-
 
 
 
