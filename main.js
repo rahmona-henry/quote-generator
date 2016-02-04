eec81cf073e22f0b936eaa6c6ad3663c24902ca3
rahmona-henry <someone@devacademy.co.nz>
2016-02-05 00:03:02 +1300

    Remove colour change

--- main.js
+++ main.js
@@ -12,16 +12,6 @@ $(document).ready(function() {
 
   button.onclick = function() { reply_click(); }
 
- function ran_col() { //function name
-                var color = '#'; // hexadecimal starting symbol
-                var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0']; //Set your colors here
-                color += letters[Math.floor(Math.random() * letters.length)];
-                document.getElementById('posts').style.background = color; // Setting the random color on your div element.
-
-
-
-
-button.onclick = function ran_col()
 
 
  });
