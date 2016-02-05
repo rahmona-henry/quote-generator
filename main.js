95fb9224ec30accb90670a3b2a3b680b00d840b1
rahmona-henry <someone@devacademy.co.nz>
2016-02-05 23:52:38 +1300

    Test colour toggle

--- main.js
+++ main.js
@@ -13,6 +13,18 @@ $(document).ready(function() {
   button.onclick = function() { reply_click(); }
 
 
+   $("button").toggle(
+    function(){$("p").css({"color": "red"});},
+    function(){$("p").css({"color": "blue"});},
+    function(){$("p").css({"color": "green"});
+});
+
+
+
+
+
+
+
 
  });
 
