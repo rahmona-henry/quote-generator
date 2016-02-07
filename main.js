b009914ac2b90e4825e23a34a7657fd454611443
rahmona-henry <someone@devacademy.co.nz>
2016-02-07 17:48:23 +1300

    Add quotes

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
-  var quotes = ['"Give me a firm spot on which to stand and I shall move the earth" - Archimedes 287-212 BC',
-                '"It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened things" - Leonardo Da Vinci 1452-1519',
-                'goodbye', 'dia duit', 'slan']; 
+  var quotes = ['"Give me a firm spot on which to stand and I shall move the earth" - Archimedes',
+                '"It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened things" - Leonardo Da Vinci',
+                '"All truths are easy to understand once they are discovered; the point is to discover them" - Galileo',
+                '"Any fool can criticise, condemn, and complain, and most fools do" - Benjamin Franklin',
+                '"When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us" - Alexander Graham Bell',
+                '"If we all did the things we are capable of, we would astound ourselves" - Thomas Edison',
+                '"Even a mistake may turn out to be the one thing necessary to a worthwhile achievement" - Henry Ford',
+                '"If you are working on something exciting, that you really care about, you dont have to be pushed" -Steve Jobs',
+                '"Sometimes it is the people no one can imagine anything of who do the things no one can imagine.” ― Alan Turing']; 
   
 
   var button = document.getElementById('button');
