8adf6359b2293d24e93a708c08a1204dfb87e083
rahmona-henry <someone@devacademy.co.nz>
2016-02-08 11:53:11 +1300

    Add new color picker

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 // Declare a variable called quotes to hold an array of inventor quotes//
+
   var quotes = ['"Give me a firm spot on which to stand and I shall move the earth" - Archimedes',
                 '"It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened things" - Leonardo Da Vinci',
                 '"All truths are easy to understand once they are discovered; the point is to discover them" - Galileo',
@@ -13,30 +14,30 @@ $(document).ready(function() {
                 '"Creativity is intelligence having fun"- Albert Einstein',
                 '"I do not think there is any thrill that can go through the human heart like that felt by the inventor as he sees some creation of the brain unfolding to success."-Nikola Tesla',
                  ]; 
-  
-// Document.getElementById returns a reference to the HTML
-// element with the Id of button. We store this reference in the declared 
-// variable called button 
+// Declare a variable called button and set it equal to Document.getElementById which returns a reference to the HTML
+// element with the Id of button.
 
   var button = document.getElementById('button');
-// A function called reply_click is created. A varibel random is declared and set equal to a Math. calculation
-// Math.random returns a floating-point number between 0 and 1.
-// This is multiplied by the number of quotes in the quotes array.
-// Math.floor is then used to convert this multiplied number to an integer.
-// getElementById is used to reference quote and display a random quote based on the variable random's calculation
-// the replreply_click is actioned everytime the quote button is clicked by the user
+
+// Create a function called reply_click. DEclare a variable called random and set it equal to a Math. calculation
+// that selects a random quote from the array quotes.
+
+
   function reply_click(){
     var random = Math.floor(Math.random() * quotes.length)
     document.getElementById('quote').innerHTML = quotes[random]
   }
-
+// the reply_click function is actioned everytime the quote button is clicked by the user
   button.onclick = function() { reply_click(); }
 
 // A function called randomColour is used to change the colour of the background everytime the quote button
 // is pressed by the user.
-  
+
+//Math.random()*256 gets a random (floating point) number from 0 to 256 (0 to 255 inclusive)
+//Using .toString(16) converts this number to hexadecimal (base 16).
   function randomColour() {
-    return '#' + Math.random().toString(16).slice(2, 8);
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+    //return '#' + Math.random().toString(16).slice(2, 8);
   }
 
   $("#button").on("click", function () {
@@ -45,3 +46,4 @@ $(document).ready(function() {
 
 });
 
+'#'+Math.floor(Math.random()*16777215).toString(16);
